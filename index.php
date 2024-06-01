675ffe985739ab9d7ab5e2a892a777a6a6a7c966
Charlie <charlie.michael.henderson@gmail.com>
2024-06-01 12:39:56 +0100

    dunno

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <?php
-
 function adminer_object()
 {
-    // Required to run any plugin.
-    include_once(__DIR__ . "/plugins/plugin.php");
+    include_once "plugins/plugin.php";
+    include_once "plugins/login-password-less.php";
 
-    // Load plugins.
-    foreach (glob(__DIR__ . "/plugins/*.php") as $file) {
-        include_once($file);
+    class AdminerCustomization extends AdminerPlugin
+    {
+        function loginFormField($name, $heading, $value)
+        {
+            if ($name == 'server') {
+                return; // server field is not needed for SQLite
+            }
+            return parent::loginFormField($name, $heading, $value);
+        }
+        function database()
+        {
+            // Replace 'database.db' with the path to your database file
+            return "mydatabase.db";
+        }
     }
 
-    $plugins = [
-        new AdminerLoginPasswordLess(password_hash("YOUR_PASSWORD_HERE", PASSWORD_DEFAULT))
-    ];
-
-    return new AdminerPlugin($plugins);
+    return new AdminerCustomization(array(
+        // Specify the plugins you want to use
+        new AdminerLoginPasswordLess(password_hash("12345-a", PASSWORD_DEFAULT)),
+    ));
 }
 
-// Include original Adminer or Adminer Editor.
-include "./adminer.php";
+include "adminer.php";
